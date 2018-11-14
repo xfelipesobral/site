c37984009b9a5f8a19cc5f6ffd1b3f305741384d
Felipe Sobral <krepper@outlook.com>
2018-11-14 01:26:07 -0200

    x

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ function bomx(){
     var d = new Date();
     if(d.getHours() >= 6 && d.getHours() < 12){
         document.write("bom dia");
+        return;
     } else if(d.getHours() >= 12 && d.getHours() < 18){
         document.write("boa tarde");
-    } else if(d.getHours() >= 18){
-        document.write("boa noite");
+        return;
     }
+    document.write("boa noite");
 }
