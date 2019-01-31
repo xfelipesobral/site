fe1a6af7da55f233cc3a6217a381bcc879a2de9d
Felipe Sobral <krepper@outlook.com>
2019-01-31 11:12:19 -0200

    UPDATE

--- js/main.js
+++ js/main.js
@@ -9,3 +9,34 @@ function bomx(){
     }
     document.write("boa noite");
 }
+
+function escolhecor(){
+    var cor = Math.floor(Math.random() * 4);
+
+    switch(cor){
+
+        case 0:
+            cor = "#002626";
+            break;
+
+        case 1:
+            cor = "#031314";
+            break;
+
+        case 2:
+            cor = "#2C3A0A";
+            break;
+        
+        case 3:
+            cor = "#421905";
+            break;
+    }
+    
+    $("#cabecalho").fadeTo("slow", 0.90);
+    $("#cabecalho").css("background-color", cor);
+    $("#cabecalho").fadeTo("slow", 1);
+}
+
+$(document).ready(function(){
+    escolhecor();
+});
