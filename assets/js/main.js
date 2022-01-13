69a690a62fad22ac4df9e236d111fe828ed98ab1
Felipe Vieira Sobral <xfelipesobral@gmail.com>
2022-01-13 02:35:51 -0300

    projetos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
-
-
+// Renderiza projeto na tela
 function projeto(img, titulo, info, url){
 
     const elemento = document.createElement("li");
@@ -12,7 +11,7 @@ function projeto(img, titulo, info, url){
                 <p>
                     ${info} 
                 </p>
-                <a class="botao" href="${url}" target="_BLANK">INFO</a>
+                <a class="botao" href="${url}" target="_BLANK">+ INFORMÇÕES</a>
             </div>
             
     `;
@@ -27,28 +26,38 @@ function projeto(img, titulo, info, url){
 projeto(
     "assets/images/projetos/zumbieinvaders.jpg",
     "Zumbie Invaders",
-    `Jogo criado na disciplina de Oficina de Computação.`,
+    `Jogo criado na disciplina de Oficina de Computação pela UFPR Palotina com a orientação do
+     professor Hélio H. Monte-Alto. Desenvolvido em Python utilizando a biblioteca PyGame. 
+     Neste jogo você manipula uma vaca que se encontra em uma infestação de zumbis e sua unica arma é o seu próprio leite,
+      para deter os zumbis você precisa acerta-los com jatos de leite. Cuidado, quando o leite acabar é necessário recarregar!
+       Este jogo é uma releitura do clássico Space Invaders.`,
     "https://github.com/xfelipesobral/ZumbieInvaders"
 )
 
 projeto(
     "assets/images/projetos/gokuvsvegeta.jpg",
-    "Goku VS Vegeta",
-    `Jogo multiplayer criado na disciplina de Redes.`,
+    "Goku VS. Vegeta",
+    `Jogo multiplayer criado na disciplina de Redes pela UFPR Palotina com a orientação do professor
+    Marcos V. O. de Assis. Desenvolvido em Python utilizando socket e a biblioteca PyGame. Neste jogo duas pessoas podem jogar em rede e 
+    batalhar para decidir quem é o melhor, você irá precisar controlar muito
+     bem seu poder para poder atacar da melhor forma possível! Este jogo é baseado na série Dragon Ball.`,
     "https://github.com/xfelipesobral/Goku-VS-Vegeta-SOCKET"
 )
 
 projeto(
     "assets/images/projetos/fecitec.jpg",
-    "Fecitec Site",
-    `Site criado para 9ª Feira de Ciências e Tecnologia (FECITEC).`,
+    "Site para FECITEC",
+    `Iniciando no projeto como voluntário e depois como bolsista, desenvolvi e manti o site para Feira de Ciências e Tecnologia (FECITEC). Atuo no projeto
+    desde sua 9ª edição.`,
     "https://github.com/xfelipesobral/fecitec"
 )
 
 projeto(
     "assets/images/projetos/webquest_colombinho.jpg",
     "USA WebQuest",
-    `Site criado na disciplina de Didática da Computação.`,
+    `Site desenvolvido para uso no estágio supervisionado do curso de computação pela UFPR Palotina com a orientação da
+    professora Eliana S. Lisbôa. O intuituo deste projeto é orientar os alunos do Ensino Fundamental no ensino de Geografia
+    sobre a história dos Estados Unidos da América. Projeto premiado na catogoria Poster no evento SLEC 2019.`,
     "https://github.com/xfelipesobral/WebQuestUSA"
 )
 
